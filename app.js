abfb30b5b0d89d950014a01ae044dc40e7e8af98
Aashoo <ashu0986ak@gmail.com>
2025-05-14 14:35:32 +0530

    added redirectfrom / to /listongs

--- app.js
+++ app.js
@@ -67,9 +67,9 @@ const sessionOptions = {
     }
 };
 
-// app.get("/", (req, res) => {
-//     res.send("I am groot !");
-// });
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 
 
 app.use(session(sessionOptions));
